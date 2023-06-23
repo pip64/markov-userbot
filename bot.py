23fd184a8bfd5895a30f78cbfccc104a8128984e
pip64 <76626915+pip64@users.noreply.github.com>
2023-06-23 18:21:01 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ app = Client("my_account")
 channels = []
 
 @app.on_message()
-async def hello(client, message):
+async def messages(client, message):
     if message.chat.id in channels:
         if len(message.text) >= 1: 
             file = open(f"{message.chat.id}.txt", "a", encoding="utf-8").write(f"{message.text}\n")
